f8cf9726fbdc9005b88ea8e2895f22ba611e4886
nikitha <nikithametta0904@gmail.com>
2024-07-05 00:01:20 +0530

    modified

--- src/app/api/displaydetails/route.ts
+++ src/app/api/displaydetails/route.ts
@@ -3,11 +3,7 @@ import pool from '../../../lib/db';
 
 export async function GET(req: NextRequest) {
   try {
-<<<<<<< HEAD
-    const [rows] = await pool.query('SELECT name, status, email, amount, date FROM users LIMIT 10');
-=======
-    const [rows] = await pool.query('SELECT name, status, email, amount, datecreated FROM data');
->>>>>>> 4696adba53cf64574db406d825e1ceb14927116b
+    const [rows] = await pool.query('SELECT name, status, email, amount, datecreated FROM data LIMIT 10');
     return NextResponse.json(rows);
   } catch (error) {
     console.error('Error fetching data:', error);
