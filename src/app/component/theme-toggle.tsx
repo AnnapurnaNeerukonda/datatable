d1e23bc9cdff7171ecfb7f73ff86083bc2fb378a
AnnapurnaNeerukonda <22b01a12b3@svcw.edu.in>
2024-07-04 23:02:05 +0530

    confirmation button and theme added

---
+++ src/app/component/theme-toggle.tsx
@@ -0,0 +1,28 @@
+// src/components/theme-toggle.tsx
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { FaSun, FaMoon } from "react-icons/fa";
+
+export default function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
+  return (
+    <button
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      className="p-1 rounded focus:outline-none"
+    >
+      {theme === "dark" ? (
+        <FaSun className="w-6 h-6 text-white-500" />
+      ) : (
+        <FaMoon className="w-6 h-6 text-gray-800" />
+      )}
+    </button>
+  );
+}
